b19e5a64beb4bc262ffa7fb76ac3d46044bfd072
Colin Geindre <colin.geindre@gmail.com>
2019-07-16 15:10:32 +0200

    changements en cours

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// MODULES
+{
 // Requetes http (appels a des api)
 var http = require('http');
 
@@ -20,11 +22,13 @@ var session = require('cookie-session');
 
 // Middleware pour le favicon
 var favicon = require('serve-favicon');
-
+}
+    
 // Stocker les dates
 var date = new Date();
 
 // VARIABLES //
+{
 //var port = 8080;
 var port = process.env.PORT;
 var msg_device, msg1_chaud, msg1_froid, msg2, msg3, msg4, msg5;
@@ -36,8 +40,10 @@ msg4 = "Il faut vraiment s'ennuyer pour faire ça..";
 msg5 = "Puisque vous continuez, on va ajouter une pub sur la page à chaque rafraichissement. Si ça, ça ne vous refroidit pas !";
 
 var msg = ["message lambda", msg2, msg3, msg4, msg5];
+}
 
 // Utilisation des middlewares
+{
 server.use(device.capture())
 .use(expressip().getIpInfoMiddleware)
 .use(favicon(__dirname+'/public/favicon.ico'))
@@ -45,29 +51,95 @@ server.use(device.capture())
   name: 'session',
   keys: ['key1', 'key2']
 }));
+}
 
 // var villes = [];
 // console.log(villes);
 
 server.get('',function(req,res){
+    // Adaptation du message en fonction du device du client
+    if(req.device.type == 'desktop') {
+        msg_device = "Appuyez sur F5 pour rafraîchir."
+    } else {
+        msg_device = "Faîtes glisser vers le bas pour rafraîchir."
+    }
+    
+    // Localisation
     var ipInfo = req.ipInfo;
-    //console.log('\n');
+    if(ipInfo.city==undefined) {
+        ipInfo.city = "Impossible de localiser le client.".red
+    }
     
-    console.log(date.getHours() + 'h' + date.getMinutes());
+    // LOG Date
+    console.log('>>>'.blue + date.getHours() + 'h' + date.getMinutes());
     
+    // Session
     if(req.session.name == undefined) {
         req.session.name = random_name();
         req.session.time = 1;
+        req.session.compteur = 1;
     
-        console.log('New visitor ! We\'ll call him "' + req.session.name.yellow + '".');
-        console.log('CITY : '.yellow + ipInfo.city);
-        console.log('DEVICE : '.yellow + req.device.type);
+        console.log('Nouveau visiteur ! On l\'appellera "'.green + req.session.name.yellow + '".'.green);
+    } else {
+        req.session.time += 1;
+        req.session.compteur += 1;
+        
+        console.log('Nouvelle connexion de ' + req.session.name.green + '. Connexion n°' + req.session.time);
+    }
+    
+    console.log('CITY : '.yellow + ipInfo.city);
+    console.log('DEVICE : '.yellow + req.device.type);
+    
+    if(ipInfo.city=="Impossible de localiser le client".red) {
         
     } else {
-        // req.session.name = 'Colin Geindre - local tester'
-        req.session.time += 1;
-        console.log(req.session.name.green + ' just logged again from ' + '\x1b[33m%s\x1b[0m', ipInfo.city + ', on ' + req.device.type.yellow +'. Its time n°' + req.session.time);
-        console.log('He camed to see : ' + req.url.yellow);
+        http.get('http://api.openweathermap.org/data/2.5/weather?q='+ undefined +',fr&appid=36c2a73dd80e36cc1c36eab251bbf29e', (resp) => {
+            let data ='';
+   
+            resp.on('data', (chunk) => {
+                data += chunk; 
+            }); 
+            
+            resp.on('end', () => {
+                var CODE = JSON.parse(data).cod.toString().green;
+                var TEMP = JSON.parse(data).main.temp;
+            
+                var meteo = parseFloat(JSON.parse(data).main.temp)-273.5;
+                meteo = Math.round(meteo*10)/10;
+                
+                var ville = 'En France.';
+                
+                if(req.session.compteur==1) {
+                    if(meteo>=25) {
+                        var premierMsg = msg1_chaud;
+                    } else {
+                        var premierMsg = msg1_froid;
+                        msg_device = "";
+                        console. log(">>>".blue + " Retour à zéro pour " +  + req.session.name.yellow + "... (cause: 'trop froid')\n".red);
+                        req.session.compteur = 0;
+                    }
+                } else if(req.session.compteur<5) {
+                    var premierMsg = msg[req.session.time-1];
+                } else {
+                    var premierMsg = msg5;
+                    console. log(">>> Destroying session of ".red + req.session.name.yellow + "... (cause: '5eme reload')\n".red);
+                    req.session = null;
+                }
+            
+                res.render('index.ejs',{weather:meteo, town:ville, rouge:red, bleu:blue, vert:green, string1:premierMsg, string2:msg_device});
+            });
+        });
+    }
+    
+    // Calcul de la couleur d'arrière plan
+    {
+    var red = meteo/40*240 + 15;
+    var blue = 255-red;
+    if(meteo>25) {
+        var green = 55;
+    } else {
+        var green = 75;
+    }
     }
     
     // GET WEATHER IN CLIENT TOWN
